4c6cfe154a2ba42376ed4c177d6d844e40912a99
mateuszdabski <mateuszdabski96@gmail.com>
2020-04-24 12:36:00 +0200

    [added] generating FCM token

--- app/src/main/java/com/example/malytom/MainActivity.kt
+++ app/src/main/java/com/example/malytom/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.malytom
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                //hiding the progressbar
+                progressbar.visibility = View.INVISIBLE
+
+                if (!task.isSuccessful) {
+                    //displaying the error if the task is unsuccessful
+                    textViewToken.text = task.exception?.message
+
+                    //stopping the further execution
+                    return@OnCompleteListener
+                }
+
+                //Getting the token if everything is fine
+                val token = task.result?.token
+
+                textViewMessage.text = "Your FCM Token is:"
+                textViewToken.text = token
+
+            })
     }
 }
